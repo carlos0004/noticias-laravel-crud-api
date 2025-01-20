ce0f5a5fe818925f722610f25306ad6b1bc0e78d
CarlosSanchez4 <csanchez@evirom.com>
2025-01-20 17:02:20 +0100

    alertas

--- resources/js/admin/admin.js
+++ resources/js/admin/admin.js
@@ -1,7 +1,10 @@
 import '../../css/admin/admin.css';
-
+import Swal from 'sweetalert2';
 
 const sidebar = document.getElementById('sidebar');
 const openClose = document.getElementById('menu');
 
-openClose.addEventListener("click", () => { sidebar.classList.toggle('show'); });
+openClose.addEventListener("click", () => {
+
+    sidebar.classList.toggle('show');
+});
