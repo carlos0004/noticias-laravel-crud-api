b24d4cdc5442829b05c1b434591234396c1308ad
CarlosSanchez4 <csanchez@evirom.com>
2025-03-18 17:21:12 +0100

    correccion

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\AutorController;
 use App\Http\Controllers\Api\CategoriaController;
 use App\Http\Controllers\Api\NoticiaController;
 use App\Http\Controllers\Api\AuthController;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
